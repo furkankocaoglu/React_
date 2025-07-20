5ac3a438a633f05625e0e3e6f9a5a3cb54ee0a04
furkankocaoglu <159638281+furkankocaoglu@users.noreply.github.com>
2025-07-20 06:44:54 +0300

    ..

---
+++ MorpheusDevelopers-React-main/ilkreactuygulamam/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        <h2>React Temellerini Öğreniyorum</h2>
+      </div>
+      <div className="App">
+        <h3>Hiyerarşik kullanım</h3>
+        <p>React Fonksiyonlarunın içerisinden mutlaka 1 tag döndürmelidir</p>
+        <p>Not: Bu alandaki her div'i ayrı birer nesne olarak düşünebilirsiniz</p>
+        <p>React sayesinde mobil programlama yaklaşımına uzaktan bir bakış atıyoruz</p>
+        <p>JSX {1+1} Fark ettiniz mi? MVC de html içerisine c# kodlarını nasıl yazıyorduk:D</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
+
+
